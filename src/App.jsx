52e62dccb1db16c04247d04320265ffd9bc4441b
SarojSahil <sahilsaroj204@gmail.com>
2025-07-04 19:26:49 +0530

    Fixed Center Alignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TeamSection from "./components/TeamSection";
 const App = () => {
 
   return (
-    <main className="flex items-center justify-center font-outfit bg-white px-4 py-8">
+    <main className="min-h-screen w-screen flex items-center justify-center font-outfit bg-white px-4 py-8">
       <TeamSection />
     </main>
   );
